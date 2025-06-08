628f71fb0aae283edeb2253fbe6bff04e25424c4
Ivan Tik <itikhonov.work@gmail.com>
2025-06-08 20:13:53 +0300

    [UPD] add func parsing text article

--- hm_01/parse_source.py
+++ hm_01/parse_source.py
@@ -3,9 +3,10 @@ import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 
-BASE_URL = "https://www.interfax.ru/business/"
+BASE_URL = "https://www.interfax.ru"
+CATEGORY = "business"
 DATETIME_FORMAT = "%Y-%m-%dT%H:%M"
-NUM_ITERATION = 5
+NUM_ITERATION = 10
 OUTPUT_FILENAME = "hm_01/interfax_business_iter_5.csv"
 
 
@@ -24,7 +25,7 @@ def extract_articles(soup: BeautifulSoup) -> list[dict]:
                     {
                         "article_time_str": article_time,
                         "article_time_datetime": datetime.datetime.strptime(article_time, DATETIME_FORMAT),
-                        "link": f"https://www.interfax.ru/{a_tag.get('href')}",
+                        "link": f"{BASE_URL}{a_tag.get('href')}",
                         "title": a_tag.get("title")
                     }
                 )
@@ -32,9 +33,21 @@ def extract_articles(soup: BeautifulSoup) -> list[dict]:
 
 
 def get_news_articles(request_params: dict = None) -> list[dict]:
-    resp = requests.get(BASE_URL, params=request_params)
+    resp = requests.get(f"{BASE_URL}/{CATEGORY}", params=request_params)
     soup = BeautifulSoup(resp.text, "html.parser")
     return extract_articles(soup)
+
+
+def get_article_info(url: str):
+    try:
+        resp = requests.get(url)
+        resp.encoding = 'cp1251'
+        soup = BeautifulSoup(resp.text, "html.parser")
+        article_block = soup.find("article")
+        return article_block.text
+    except Exception as err:
+        print(f"Problems with parsing {url}")
+        return None
 
 
 all_articles = sorted(get_news_articles(), key=lambda x: x["article_time_datetime"], reverse=True)
@@ -52,4 +65,10 @@ for i in range(1, NUM_ITERATION + 1):
         all_articles = sorted(all_articles, key=lambda x: x["article_time_datetime"], reverse=True)
 
 print(f"Total articles collected: {len(all_articles)}")
+
+for idx, article in enumerate(all_articles):
+    print(f"{idx} Parse: {article["link"]}")
+    article_text = get_article_info(article["link"])
+    article["text"] = article_text
+
 pd.DataFrame(all_articles).to_csv(OUTPUT_FILENAME, encoding="utf-8", index=False)
